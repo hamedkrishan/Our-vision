5f9675287fb5eb038716830c8c528fb0025e80e7
hamedkrishan <hamedkrishan676@gmail.com>
2025-02-07 21:48:10 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <div className="unique-vision-container">
+          <div className="unique-circle unique-mission">
+            <h2>Mission</h2>
+            <p>
+              Orange Coding School provides top coding education, making
+              programming fun, practical, and accessible while preparing
+              students for tech careers with real-world skills.
+            </p>
+          </div>
+          <div className="unique-circle unique-vision">
+            <h2>Vision</h2>
+            <p>
+              Orange Coding School makes coding education accessible and
+              practical, preparing learners to innovate, solve problems, and
+              lead in tech.
+            </p>
+          </div>
+          <div className="unique-circle unique-goals">
+            <h2>Goals</h2>
+            <p>
+              Orange Coding School aims to equip students with real-world coding
+              skills, bridge education with industry, and create future-ready
+              tech professionals.
+            </p>
+          </div>
+          <div className="unique-circle unique-values">
+            <h2>Values</h2>
+            <p>
+              Orange Coding School values innovation, accessibility, hands-on
+              learning, continuous growth, community, integrity, and
+              collaboration to empower future tech leaders.
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
